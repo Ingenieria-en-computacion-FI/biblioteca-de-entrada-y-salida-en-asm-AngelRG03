977db50e64d213429a112cc8ea404c4a38a9bac4
AngelRG03 <mike12rg@gmail.com>
2026-03-12 00:07:46 -0600

    Update newline.asm

--- src/newline.asm
+++ src/newline.asm
@@ -2,22 +2,22 @@ extern print_char
 global newline
 
 SECTION .text
-; ---------------------------------
+
+
 ; newline
-; Imprime un salto de línea '\n'
-; Reutiliza print_char
-; ---------------------------------
+
 newline:
     push ebp
     mov ebp, esp
 
-    push eax                ; preservar EAX
+    push eax
 
-    mov al, 0x0A            ; '\n' = 10 en ASCII
+    mov al, 0x0A            ; '\n'
     call print_char
 
     pop eax
 
     mov esp, ebp
     pop ebp
+    ret
     ret
