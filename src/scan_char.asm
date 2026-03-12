45b79d4cc83917190f112db46ce651c79672e9ef
AngelRG03 <mike12rg@gmail.com>
2026-03-12 00:06:04 -0600

    Update scan_char.asm

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -1,12 +1,10 @@
 SECTION .bss
-char_buffer resb 1
-
+sc_buf resb 1        
 SECTION .text
 global scan_char
 ; ---------------------------------
 ; scan_char
-; Salida:
-;   AL = caracter leído
+; Salida: AL = carácter leído
 ; ---------------------------------
 scan_char:
     push ebp
@@ -16,15 +14,13 @@ scan_char:
     push ecx
     push edx
 
-    ; read(fd=0, &char_buffer, 1)
-    mov eax, 3              ; syscall read
+    mov eax, 3              ; sys_read
     mov ebx, 0              ; fd = stdin
-    mov ecx, char_buffer    ; dirección del buffer
+    mov ecx, sc_buf         ; buffer
     mov edx, 1              ; 1 byte
     int 0x80
 
-    ; Devolver el caracter leído en AL
-    mov al, [char_buffer]
+    mov al, [sc_buf]        
 
     pop edx
     pop ecx
