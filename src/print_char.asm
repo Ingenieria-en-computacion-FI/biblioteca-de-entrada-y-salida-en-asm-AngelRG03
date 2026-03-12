18ec9e7afd78ef9ba38087a66424419d31aa90f5
AngelRG03 <mike12rg@gmail.com>
2026-03-11 23:57:29 -0600

    Implement print_char function to output a character

--- src/print_char.asm
+++ src/print_char.asm
@@ -1,23 +1,37 @@
+SECTION .bss
+char_buffer resb 1
+
 SECTION .text
 global print_char
-
 ; ---------------------------------
 ; print_char
 ; Entrada:
 ;   AL = caracter a imprimir
 ; ---------------------------------
-
 print_char:
-
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. Guardar el caracter en memoria
-    ; 2. Usar syscall write
-    ; 3. Imprimir 1 byte
+    ; Guardar registros que usaremos
+    push eax
+    push ebx
+    push ecx
+    push edx
 
-    ; write(fd=1, buffer, 1)
+    ; Guardar el caracter en memoria
+    mov [char_buffer], al
+
+    ; write(fd=1, &char_buffer, 1)
+    mov eax, 4              ; syscall write
+    mov ebx, 1              ; fd = stdout
+    mov ecx, char_buffer    ; dirección del buffer
+    mov edx, 1              ; 1 byte
+    int 0x80
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
 
     mov esp, ebp
     pop ebp
