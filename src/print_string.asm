3cf3ed54b92607f2f5a65d561306359d42fef632
AngelRG03 <mike12rg@gmail.com>
2026-03-12 00:06:32 -0600

    Update print_string.asm

--- src/print_string.asm
+++ src/print_string.asm
@@ -2,39 +2,39 @@ SECTION .text
 global print_string
 ; ---------------------------------
 ; print_string
-; Entrada:
-;   EAX = dirección de cadena terminada en 0
+; Entrada: EAX = dirección de cadena terminada en 0
 ; ---------------------------------
 print_string:
     push ebp
     mov ebp, esp
 
-    push eax
     push ebx
     push ecx
     push edx
+    push esi
 
-    ; Calcular longitud de la cadena
-    mov ecx, eax            ; ECX = puntero al inicio
-    mov edx, 0              ; contador de longitud
-.count_loop:
-    cmp byte [ecx + edx], 0 ; ¿terminador nulo?
-    je  .count_done
+    mov esi, eax          
+
+    ; calcular longitud
+    mov edx, 0
+.len_loop:
+    cmp byte [esi + edx], 0
+    je  .len_done
     inc edx
-    jmp .count_loop
-.count_done:
-    ; EDX = longitud, EAX = dirección
+    jmp .len_loop
+.len_done:
+    ; EDX = longitud, ESI = dirección
 
-    ; write(fd=1, cadena, longitud)
-    mov ebx, 1              ; fd = stdout
-    mov ecx, eax            ; dirección de la cadena
-    mov eax, 4              ; syscall write
+    mov eax, 4         
+    mov ebx, 1           
+    mov ecx, esi           
+    ; edx ya tiene la longitud
     int 0x80
 
+    pop esi
     pop edx
     pop ecx
     pop ebx
-    pop eax
 
     mov esp, ebp
     pop ebp
