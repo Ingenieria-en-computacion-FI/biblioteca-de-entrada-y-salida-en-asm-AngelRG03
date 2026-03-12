59491eed88b894b37bba5f123e01dcac233cb7b3
AngelRG03 <mike12rg@gmail.com>
2026-03-11 23:57:45 -0600

    Add syscall to read a character from stdin
    Implemented character reading from stdin using syscall.

--- src/scan_char.asm
+++ src/scan_char.asm
@@ -3,22 +3,32 @@ char_buffer resb 1
 
 SECTION .text
 global scan_char
-
 ; ---------------------------------
 ; scan_char
 ; Salida:
 ;   AL = caracter leído
 ; ---------------------------------
-
 scan_char:
-
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. usar syscall read
-    ; 2. leer 1 byte desde stdin
-    ; 3. devolverlo en AL
+    push ebx
+    push ecx
+    push edx
+
+    ; read(fd=0, &char_buffer, 1)
+    mov eax, 3              ; syscall read
+    mov ebx, 0              ; fd = stdin
+    mov ecx, char_buffer    ; dirección del buffer
+    mov edx, 1              ; 1 byte
+    int 0x80
+
+    ; Devolver el caracter leído en AL
+    mov al, [char_buffer]
+
+    pop edx
+    pop ecx
+    pop ebx
 
     mov esp, ebp
     pop ebp
