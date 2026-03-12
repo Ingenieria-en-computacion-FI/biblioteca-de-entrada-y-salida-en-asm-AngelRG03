d0a8d2480cd12edf173eb711d1f61177ce0e94e6
AngelRG03 <mike12rg@gmail.com>
2026-03-11 23:58:04 -0600

    Implement string length calculation and print syscall
    Added functionality to calculate string length and print it using syscall.

--- src/print_string.asm
+++ src/print_string.asm
@@ -1,21 +1,40 @@
 SECTION .text
 global print_string
-
 ; ---------------------------------
 ; print_string
 ; Entrada:
-;   EAX = direccion de cadena
-;   cadena terminada en 0
+;   EAX = dirección de cadena terminada en 0
 ; ---------------------------------
-
 print_string:
-
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. calcular longitud
-    ; 2. syscall write
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    ; Calcular longitud de la cadena
+    mov ecx, eax            ; ECX = puntero al inicio
+    mov edx, 0              ; contador de longitud
+.count_loop:
+    cmp byte [ecx + edx], 0 ; ¿terminador nulo?
+    je  .count_done
+    inc edx
+    jmp .count_loop
+.count_done:
+    ; EDX = longitud, EAX = dirección
+
+    ; write(fd=1, cadena, longitud)
+    mov ebx, 1              ; fd = stdout
+    mov ecx, eax            ; dirección de la cadena
+    mov eax, 4              ; syscall write
+    int 0x80
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
 
     mov esp, ebp
     pop ebp
