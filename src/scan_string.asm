51a1b4cf30e9825ce755906b38ac74a61e448d6e
AngelRG03 <mike12rg@gmail.com>
2026-03-12 00:07:16 -0600

    Update scan_string.asm

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -10,47 +10,27 @@ scan_string:
     push ebp
     mov ebp, esp
 
-    push eax
     push ebx
     push ecx
     push edx
+    push esi
 
-    ; read(fd=0, buffer, tamaño_máximo)
-    mov ecx, eax            ; dirección del buffer destino
-    mov edx, ebx            ; tamaño máximo
-    mov eax, 3              ; syscall read
-    mov ebx, 0              ; fd = stdin
-    int 0x80
+    mov esi, eax          
+    mov ecx, eax        
+    mov edx, ebx     
 
-    ; EAX ahora contiene bytes leídos
-    ; Agregar terminador nulo al final
-    ; La posición del terminador = buffer + bytes_leídos
-    pop edx                 ; restaurar para reusar edx
-    pop ecx                 ; restaurar ecx
-    pop ebx                 ; restaurar ebx (buffer original en stack)
-    pop eax                 ; restaurar eax (dirección del buffer)
+    mov eax, 3          
+    mov ebx, 0     
+    int 0x80             
 
-    ; Recalcular: guardar dirección y longitud antes del ret
-    push eax
-    push ebx
-    push ecx
-    push edx
 
-    mov ecx, eax            ; buffer destino
-    mov edx, ebx            ; tamaño máximo
-    mov eax, 3
-    mov ebx, 0
-    int 0x80
-    ; EAX = bytes leídos
-    ; Quitar el '\n' si está al final
-    mov ecx, [ebp + 8]      ; buffer destino (primer arg)
-    add ecx, eax            ; apuntar al final
-    mov byte [ecx], 0       ; terminador nulo
 
+    mov byte [esi + eax], 0
+
+    pop esi
     pop edx
     pop ecx
     pop ebx
-    pop eax
 
     mov esp, ebp
     pop ebp
