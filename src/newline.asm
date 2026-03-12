4628f20f3b05428c03e7fc46bab2df6c9dbdab95
AngelRG03 <mike12rg@gmail.com>
2026-03-11 23:59:52 -0600

    Implement newline function to print newline character

--- src/newline.asm
+++ src/newline.asm
@@ -2,14 +2,21 @@ extern print_char
 global newline
 
 SECTION .text
-
+; ---------------------------------
+; newline
+; Imprime un salto de línea '\n'
+; Reutiliza print_char
+; ---------------------------------
 newline:
-
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; imprimir '\n'
+    push eax                ; preservar EAX
+
+    mov al, 0x0A            ; '\n' = 10 en ASCII
+    call print_char
+
+    pop eax
 
     mov esp, ebp
     pop ebp
